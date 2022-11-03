51d75eb0644547891de09fa7d544de8c5502fc83
karanjalight <karanjalight@gmail.com>
2022-11-03 16:01:01 +0300

    adding the backend and frontend

---
+++ state_of_kiambu/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StateOfKiambuConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "state_of_kiambu"
---
+++ state_of_kiambu/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+def home(request):	
+
+	return render(request, "index.html")
+
+def article(request):	
+
+	return render(request, "article.html")
